da0c925a27b0c172da8d4b59a9f3ba2309f50dcb
Florencia Valladares <fvalladares1@uc.cl>
2019-04-23 17:06:30 -0400

    basic posts show

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,31 @@
-console.log('Hola mundo!');
+
+const posts = [
+  {
+    author: "Florencia",
+    description: "Post 1",
+  }, 
+  {
+    author: "Thomas",
+    description: "Post 2",
+  }
+]
+
+const addPost = () => {
+  const author = document.getElementById("author-input").value;
+  const description = document.getElementById("description-input").value;
+  // TODO: send author and description to firebase
+};
+
+const button = document.getElementById("post-button");
+button.addEventListener("click", addPost);
+
+const postsContainer = document.getElementById("posts-container");
+postsContainer.innerHTML = posts.map(post => `
+  <div class="post">
+    <p class="post-author">${post.author}</p> 
+    <p class="post-description">${post.description}</p> 
+  </div>
+`).join('');
 
 if (module.hot) {
   module.hot.accept();
