3e6ab9d9207b2fd1c8dfd0cb4b1e81525d8899ba
Florencia Valladares <fvalladares1@uc.cl>
2019-04-23 23:07:17 -0400

    register sw

--- src/app/index.js
+++ src/app/index.js
@@ -30,6 +30,19 @@ document.addEventListener("DOMContentLoaded", function() {
 `).join('');
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registration completed with scope: ',
+        registration.scope)
+    }, (err) => {
+      console.log('Service Worker registration failed', err)
+}) })
+} else {
+  console.log('Service Workers not supported')
+}
+
 if (module.hot) {
   module.hot.accept();
 }
