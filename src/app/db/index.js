6ec04f4d33c6da2204a3f3abf0aef07ed16b155f
Florencia Valladares <fvalladares1@uc.cl>
2019-04-24 12:40:08 -0400

    read posts online

--- src/app/db/index.js
+++ src/app/db/index.js
@@ -1,10 +1,21 @@
 import firebase from "firebase";
 import config from "./config";
 
+const dbName = "postsDB";
 
 export const configureDB = () => {
   // Initialize Firebase
   firebase.initializeApp(config);
+
+  // Initialize indexedDB
+  var request = indexedDB.open(dbName, 2);
+
+  request.onupgradeneeded = (event) => {
+    var db = event.target.result;
+    var objectStore = db.createObjectStore("posts", { autoIncrement: true });
+    objectStore.createIndex("author", "author", { unique: false });
+    objectStore.createIndex("body", "body", { unique: true });
+  };
 };
 
 export const writeNewPost = (username, body, callback) => {
@@ -24,12 +35,55 @@ export const writeNewPost = (username, body, callback) => {
   return firebase.database().ref('/posts/' + newPostKey).set(postData, callback);
 };
 
+
+export const loadPostIndexedDB = (callback) => {
+  const request = indexedDB.open(dbName, 2);
+  request.onsuccess = event => {
+    const db = event.target.result;
+    const transaction = db.transaction(["posts"], "readwrite");
+    const objectStore = transaction.objectStore("posts");
+
+    const postsRequest = objectStore.getAll();
+    postsRequest.onsuccess = event => {
+      callback(event.target.result);
+    }
+  }
+}
+
+export const addPostsIndexedDB = (posts) => {
+  const request = indexedDB.open(dbName, 2);
+  request.onsuccess = event => {
+    const db = event.target.result;
+    const transaction = db.transaction(["posts"], "readwrite");
+    const objectStore = transaction.objectStore("posts");
+
+    const postsRequest = objectStore.getAll();
+    postsRequest.onsuccess = event => {
+      const dbPosts = event.target.result;
+      if (dbPosts.length !== posts.length) {
+        dbPosts.forEach((post, i) => {
+          objectStore.delete(i);
+        })
+        posts.forEach((posts) => {
+          objectStore.add(posts);
+        })
+      }
+    }
+  }
+}
+
 export const configurePostUpdate = (callback) => {
+  
+  loadPostIndexedDB(callback);
+
   firebase.database().ref().child('posts').on('value', snapshot => {
     const posts = [];
     snapshot.forEach(childSnapshot => {
       posts.push(childSnapshot.val());
     });
+    
+    addPostsIndexedDB(posts);
+    
     callback(posts);
   })
 };
